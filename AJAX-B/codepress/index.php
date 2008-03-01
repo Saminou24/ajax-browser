9b406929289cf33893bb9cf086d37fd953cd7e88
alban.lopez <alban.lopez@2280f851-b520-0410-b8c1-6b12d3945160>
2008-03-01 19:01:22 +0000

    Codepress editor beta 4

---
+++ AJAX-B/codepress/index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html>
+<head>
+	<title>CodePress - Real Time Syntax Highlighting Editor written in JavaScript</title>
+<!--	<script type="text/javascript">
+		ABS900='erreur !';
+		function SaveMe (Data64)
+		{
+			RQT.get
+			(ServActPage,
+				{
+					parameters:'mode=request&cpsave=< ? echo $view;?>&data64='+Data64,
+					onEnd:'ID("saved").innerHTML="Last Saved : "+request.responseText;'
+				}
+			);
+		}
+	</script>
+	<script type="text/javascript" src="< ?php echo INSTAL_DIR; ?>scripts/Common.js"></script>
+	<script type="text/javascript" src="< ?php echo INSTAL_DIR; ?>scripts/UTF8Base64.js"></script>-->
+	<script type="text/javascript" src="<?php echo INSTAL_DIR; ?>codepress/codepress.js"></script>
+	<style>
+	body {
+		color:#000;
+		background-color:white;
+		font:15px georgia, "Lucida Grande", "Lucida Sans Unicode", Arial, Helvetica, sans-serif;
+		letter-spacing:0.01em;
+	}
+	html, body, textarea {
+		position: absolute;
+		width:100%;
+		height:100%;
+		padding:0px;
+		margin:0px;
+		border:0px;
+		overflow: hidden;
+	}
+	iframe {
+		position: absolute;
+		width:100%;
+		height:100%;
+		padding:0px;
+		margin:0px;
+		border:0px;
+		top:0px;
+	}
+	.top {
+		position:absolute;
+		width:100%;
+		top:0px;
+		float:right;
+		text-align:right;
+	}
+	.right {padding-right:20px;}
+	</style>
+</head>
+<body>
+	<div class="top">
+		<div class="right">
+		<?php
+			is_array($lst = glob (INSTAL_DIR.'codepress/languages/*.css'))?$lst:array();
+			foreach ( as $key=>$val)
+				echo '<button onclick="cp1.edit(cp1.value, \''.pathinfo($val, PATHINFO_FILENAME).'\');">'.pathinfo($val, PATHINFO_FILENAME).'</button>';
+		?>
+		</div>
+	</div><!--< ?php echo pathinfo($file, PATHINFO_EXTENSION);?>-->
+	<textarea id="cp1" class="codepress php">
+	<?php
+		echo encode64(file_get_contents($file));
+	?>
+	</textarea>
+</body>
+</html>
