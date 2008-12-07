d11972ec467ac862f07297b2f1b562f9b2254107
alban.lopez <alban.lopez@2280f851-b520-0410-b8c1-6b12d3945160>
2008-12-07 00:31:53 +0000

    Edit Area restore

--- trunk/AJAX-B/edit_area/keyboard.js
+++ trunk/AJAX-B/edit_area/keyboard.js
@@ -1,8 +1,8 @@
 var EA_keys = {8:"Retour arriere",9:"Tabulation",12:"Milieu (pave numerique)",13:"Entrer",16:"Shift",17:"Ctrl",18:"Alt",19:"Pause",20:"Verr Maj",27:"Esc",32:"Space",33:"Page up",34:"Page down",35:"End",36:"Begin",37:"Left",38:"Up",39:"Right",40:"Down",44:"Impr ecran",45:"Inser",46:"Suppr",91:"Menu Demarrer Windows / touche pomme Mac",92:"Menu Demarrer Windows",93:"Menu contextuel Windows",112:"F1",113:"F2",114:"F3",115:"F4",116:"F5",117:"F6",118:"F7",119:"F8",120:"F9",121:"F10",122:"F11",123:"F12",144:"Verr Num",145:"Arret defil"};
 
+
+
 function keyDown(e){
-	top.document.title="*"+top.document.defaultView._title;
-	
 	if(!e){	// if IE
 		e=event;
 	}
@@ -112,7 +112,9 @@ function keyDown(e){
 		return false;
 	}
 	//alert("Test: "+ letter + " ("+e.keyCode+") ALT: "+ AltPressed(e) + " CTRL "+ CtrlPressed(e) + " SHIFT "+ ShiftPressed(e));
+	
 	return true;
+	
 };
 
 
