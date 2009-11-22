479be02e710fb52b5a5c34ea4ac9eb666361f0df
alban.lopez <alban.lopez@2280f851-b520-0410-b8c1-6b12d3945160>
2009-11-22 20:02:13 +0000

    first coding

---
+++ trunk/AB2/ab2.php
@@ -0,0 +1,10 @@
+<?php
+/*# -------- DECLARE -------- */
+	define("INSTALLPATH", "./ab2/");
+	define("LOGPATH", "./log/");
+	define("MINIPATH", "./mini/");
+	define("MODPATH", INSTALLPATH."modules/");
+	define("PICPATH", INSTALLPATH."pics/");
+
+	include (INSTALLPATH.'kernel.php');
+?>
